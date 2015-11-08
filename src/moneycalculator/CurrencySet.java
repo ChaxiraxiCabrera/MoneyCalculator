82248aa088a4c11f597a611c3fbb6f555883425a
Chaxi <Chaxi@192.168.1.13>
2015-11-08 10:14:09 +0000

    Cuarta version

---
+++ src/moneycalculator/CurrencySet.java
@@ -0,0 +1,22 @@
+package moneycalculator;
+
+import java.util.ArrayList;
+
+public class CurrencySet {
+    private ArrayList<Currency> list = new ArrayList<>();
+    
+    public void add(Currency currency){
+        list.add(currency);
+    }
+
+    public Currency get (String text){
+        for (Currency currency : list) {
+            if(currency.getCode().equalsIgnoreCase(text) 
+                    || currency.getName().contains(text) 
+                    || currency.getSymbol().equals(text)){
+                return currency;
+            }
+        }
+        return null;
+    }
+}
